8e7a45019475cbe0c1faff10f9bd755942591e49
Devin Chong <dvnchong88@gmail.com>
2022-05-16 16:27:33 +0900

    fixed the models, belongs to user and belongs to animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,3 @@
 class Animal < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :animal_id
-  belongs_to :user_id
+  belongs_to :animal
+  belongs_to :user
 end
